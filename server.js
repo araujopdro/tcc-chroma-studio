14667b2dd426b318c4c83d9e17adc25039203c20
Pedro Machado Araujo <pedroa@spi.local>
2017-08-31 14:32:42 -0300

    server fix 2

--- server.js
+++ server.js
@@ -1,46 +1,37 @@
 
 // Dependencies
-var express = require('express');
-var mongoose = require('mongoose');
-var socket = require('socket.io');
+var express = require('express'),
+	mongoose = require('mongoose'),
+	app = express(),
+	server = require('http').createServer(app),
+    io = require('socket.io').listen(server),
+    body_parser = require('body-parser');
 
-var body_parser = require('body-parser');
 
-// MongoDB
+var mongodbURI = "mongodb://araujopdro:Puertoric0@ds149743.mlab.com:49743/tcc-chroma-studio"
 mongoose.connect('localhost/tcc_server');
 
-// Express
-var app = express();
 app.use(body_parser.urlencoded({ extended: true }));
 app.use(body_parser.json());
-
 
 
 // Routes
 app.use('/api', require('./routes/api'));
 
-
-// Start Server 
-var port = process.env.PORT || 3000
-
-//var server = app.listen(3000);
-var server = app.listen(port, function() {
+var port = process.env.PORT || 3000;
+server.listen(port, function() {
     console.log("App is running on port " + port);
 });
 
 console.log('Pudim de Leite 2');
 
-
-// Socket Setup
-var io = socket(server);
-
-io.on('connection', function(socket){
+io.sockets.on('connection', function(socket){
 	console.log('Connect socket', socket.id);
 
 
-	socket.on('pudim', function(data){
-		console.log('pudim stuff');
-		console.log(data);
-		io.sockets.emit('pudim', data);
-	});
+	// socket.on('pudim', function(data){
+	// 	console.log('pudim stuff');
+	// 	console.log(data);
+	// 	io.sockets.emit('pudim', data);
+	// });
 });
